998a7bd353f776568606244ec2b17cb5aba78fc8
kelly <13122395209@163.com>
2020-12-30 14:19:39 +0800

    测试开发

--- Python/JD_auto.py
+++ Python/JD_auto.py
@@ -6,8 +6,8 @@ desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '6.0.1'
 desired_caps['deviceName'] = 'MI_4LTE'
-desired_caps['appPackage'] = 'com.jingdong.app.mall'
-desired_caps['appActivity'] = '.MainFrameActivity'
+desired_caps['appPackage'] = 'com.jingdong.app.mall'#com.taobao.taobao  com.jingdong.app.mall
+desired_caps['appActivity'] = '.main.MainActivity' #com.taobao.tao.TBMainActivity  main.MainFrameActivity
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 wait = WebDriverWait(driver,30)
